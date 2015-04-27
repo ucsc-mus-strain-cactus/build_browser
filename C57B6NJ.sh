627844d82084bb3d7c8cc4b77db513e7cd146c06
ifiddes <ian.t.fiddes@gmail.com>
2015-04-27 16:04:25 -0700

    .

--- C57B6NJ.sh
+++ C57B6NJ.sh
@@ -1,1 +1,8 @@
-export db=C57B6NJ
+GENOME=$1
+TWOBIT=/cluster/home/ifiddes/ifiddes_hive/comparativeAnnotator/pipeline_data/assemblies/1411/$1.2bit
+SCI_NAME="Mus musculus"
+
+export DB=$1
+
+mkdir ${DB}
+cp ${TWOBIT} ${DB}/${DB}.2bit
