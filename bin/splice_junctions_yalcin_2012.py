2ce45660c56d0ed3b8376ed8c7a2415d0e4add7d
ifiddes <ian.t.fiddes@gmail.com>
2015-06-02 11:25:55 -0700

    sv tracks

---
+++ bin/splice_junctions_yalcin_2012.py
@@ -0,0 +1,55 @@
+"""
+Produces trackDb files for the chr19 splice junctions seen in Yalcin et al 2012
+Which are present at /hive/groups/recon/projs/mus_strain_cactus/data/yalcin_structural_variants
+These BEDs are lifted over from mm9 to mm10 by Joel
+"""
+
+import sys
+import os
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--assembly_version', help='genome assembly version (1504, etc)', required=True)
+    parser.add_argument('--ref_strain', help='reference strain (C57B6J)', required=True)
+    return parser.parse_args()
+
+
+bed_dir = "/hive/groups/recon/projs/mus_strain_cactus/data/yalcin_structural_variants"
+
+
+composite_track = """track yalcin_structural_variants
+compositeTrack on
+shortLabel Structural Variants
+longLabel Gold Standard Structural Variants (Yalcin et al 2012)
+type bam
+allButtonPair on
+dragAndDrop subTracks
+
+"""
+
+per_bed_track = """    track {genome}_yalcin_svs
+    shortLabel {genome}
+    longLabel {genome} Structural Variants
+    type bed 4
+    visibility hide
+
+"""
+
+def main():
+    args = parse_args()
+    trackDb_path = os.path.join("trackDb", args.ref_strain, "Mus" + args.ref_strain + "_" + args.assembly_version)
+    with open(os.path.join(trackDb_path, "spliceJunctions.trackDb.ra"), "w") as outf:
+        outf.write(composite_track)
+        for bed in os.listdir(bed_dir):
+            assert bed.endswith(".bed")
+            genome = bed.split("_")[0]
+            outf.write(per_bed_track.format(genome=genome))
+    tmp = "".join(open(os.path.join(trackDb_path, "trackDb.ra")).readlines())
+    if "include spliceJunctions.trackDb.ra" not in tmp:
+        with open(os.path.join(trackDb_path, "trackDb.ra"), "a") as outf:
+            outf.write("\ninclude spliceJunctions.trackDb.ra\n")
+
+
+
+if __name__ == "__main__":
+    main()
